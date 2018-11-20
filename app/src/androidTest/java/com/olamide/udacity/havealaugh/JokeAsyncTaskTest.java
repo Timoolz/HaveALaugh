d11f07a0e947207baaab51e9eeb7bc06cdcbffdb
Olamide.Laleye <olamide.laleye@interswitchng.com>
2018-11-20 19:38:25 +0100

    Create and run tests

---
+++ app/src/androidTest/java/com/olamide/udacity/havealaugh/JokeAsyncTaskTest.java
@@ -0,0 +1,34 @@
+package com.olamide.udacity.havealaugh;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.olamide.udacity.havealaugh.backend.myApi.model.Joke;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class JokeAsyncTaskTest  {
+
+    @Test
+    public void testDoInBackground() throws InterruptedException {
+        Thread.sleep(7000);
+
+        final JokeAsyncTask jokeAsyncTask = new JokeAsyncTask(new JokeAsynctaskInterface()
+        {
+            @Override
+            public void didplayJoke(Joke joke) {
+
+                assertNotNull(joke);
+                assertNotNull(joke.getContent());
+            }
+
+        });
+
+        jokeAsyncTask.execute();
+
+    }
+}
