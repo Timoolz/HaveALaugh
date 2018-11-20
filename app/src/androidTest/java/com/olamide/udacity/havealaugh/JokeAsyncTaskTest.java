7414e1dc5d8dddcdbdf081d3a4af30f76bdba235
Olamide.Laleye <olamide.laleye@interswitchng.com>
2018-11-20 19:42:12 +0100

    Create and run tests

--- app/src/androidTest/java/com/olamide/udacity/havealaugh/JokeAsyncTaskTest.java
+++ app/src/androidTest/java/com/olamide/udacity/havealaugh/JokeAsyncTaskTest.java
@@ -24,6 +24,7 @@ public class JokeAsyncTaskTest  {
 
                 assertNotNull(joke);
                 assertNotNull(joke.getContent());
+                assertNotEquals(joke.getContent(),"");
             }
 
         });
